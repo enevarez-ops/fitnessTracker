7ea5c096f2b93d503a0e797b661eddf4d977adbe
Efrain Nevarez <efrain.nevarezjr@gmail.com>
2021-03-03 19:47:39 -0600

    added router get

--- routes/api.js
+++ routes/api.js
@@ -20,3 +20,20 @@ router.put("/api/workouts/:id", (req, res) => {
     })
 });
 
+router.get("/api/workouts", (req, res) => {
+    Workout.aggregate([
+        { 
+            $addFields:{
+                totalDuration: {
+                    $sum: "$exercises.duration"
+                }
+            }
+        }
+    ]).sort({
+       _id: -1 
+    }).limit(7).then(workout =>{
+        res.json(workout)
+    }).catch(err => {
+        res.json(err)
+    })
+});
